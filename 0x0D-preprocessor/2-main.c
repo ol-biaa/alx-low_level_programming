528d446985f8844262b506b68757d2be241e6a8d
ol-biaa <129224295+ol-biaa@users.noreply.github.com>
2023-09-13 00:51:38 +0300

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Description: prints the name of the file it was compiled from, followed by a new line.
+ * main - prints the name of the file
  *
  * Return: Always 0 (Success)
-*/
-
+ */
 int main(void)
 {
 	printf("%s\n", __FILE__);
