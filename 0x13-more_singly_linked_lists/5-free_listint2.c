5ca22745ac64c05b40a2d04a48922e2d5bb4c27e
ol-biaa <129224295+ol-biaa@users.noreply.github.com>
2023-09-26 23:15:56 +0300

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,26 +3,21 @@
 /**
  * free_listint2 - frees a linked list
  * @head: pointer to the listint_t list to be freed
- *
+ */
 void free_listint2(listint_t **head)
 {
-	listint_t *current, *temp;
+	listint_t *temp;
 
-	if (head != NULL)
+	if (head == NULL)
+		return;
+
+	while (*head)
 	{
-		/*set head addr to current*/
-		current = *head;
-
-		/*iterate through the whole list*/
-		/*while setting the current node to temp*/
-		while ((temp = current) != NULL)
-		{
-			/*set next node to curretnt*/
-			current = current->next;
-			/*free temp, that is the current node*/
-			free(temp);
-		}
-
-		*head = NULL;
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
 	}
+
+	*head = NULL;
 }
+
